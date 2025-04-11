94bb6a7a40fcc2266caae9444893d6f326e6a940
Ksenia-rgb <ksenialymova@mail.ru>
2025-04-11 16:50:12 +0300

    add functions, input process

--- alymova.ksenia/K6/main.cpp
+++ alymova.ksenia/K6/main.cpp
@@ -1,3 +1,47 @@
-int main()
-{}
+#include <iostream>
+#include "tree-utils.hpp"
 
+int main()
+{
+  size_t cnt;
+  std::cin >> cnt;
+  if (std::cin.fail())
+  {
+    std::cerr << "<INVAID INPUT>\n";
+    return 1;
+  }
+  BiTree< int >* root = nullptr;
+  try
+  {
+    if (cnt > 0)
+    {
+      root = create< int, std::less< int > >(std::cin, cnt, std::less< int >());
+    }
+    std::string command;
+    int num;
+    while (std::cin >> command >> num)
+    {
+      BiTree< int >* new_root = find(root, num, std::less< int >());
+      if (command == "right")
+      {
+        new_root = rotate_right(new_root);
+      }
+      else if (command == "left")
+      {
+        new_root = rotate_left(new_root);
+      }
+      std::cout << new_root->data << '\n';
+    }
+    if (!std::cin.eof())
+    {
+      throw std::invalid_argument("<INVALID COMMAND>");
+    }
+  }
+  catch(const std::exception& e)
+  {
+    clear(root);
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  clear(root);
+}
---
+++ alymova.ksenia/K6/tree-utils.hpp
@@ -0,0 +1,155 @@
+#ifndef TREE_UTILS_HPP
+#define TREE_UTILS_HPP
+#include <exception>
+
+template< class T >
+struct BiTree {
+  T data;
+  BiTree< T >* left;
+  BiTree< T >* right;
+  BiTree< T >* parent;
+};
+
+template< class T, class Comparator >
+BiTree< T >* create(std::istream& in, size_t cnt, Comparator comp);
+
+template< class T >
+BiTree< T >* rotate_right(BiTree< T >* root);
+
+template< class T >
+BiTree< T >* rotate_left(BiTree< T >* root);
+
+template< class T, class Comparator >
+BiTree< T >* find(BiTree< T >* root, const T & value, Comparator comp);
+
+template< class T >
+void clear(BiTree< T >* root);
+
+template< class T, class Comparator >
+BiTree< T >* create(std::istream& in, size_t cnt, Comparator comp)
+{
+  BiTree< T >* root = new BiTree< T >{0, nullptr, nullptr, nullptr};
+  in >> root->data;
+  for (size_t i = 1; i < cnt && in; i++)
+  {
+    BiTree< T >* node;
+    try
+    {
+      node = new BiTree< T >{0, nullptr, nullptr, nullptr};
+    }
+    catch (const std::bad_alloc& e)
+    {
+      clear(root);
+      throw;
+    }
+    in >> node->data;
+    BiTree< T >* tmp = root;
+    while (tmp->left && tmp->right)
+    {
+      if (comp(node->data, tmp->data))
+      {
+        tmp = tmp->left;
+      }
+      else
+      {
+        tmp = tmp->right;
+      }
+    }
+    if (comp(node->data, tmp->data))
+    {
+      tmp->left = node;
+    }
+    else
+    {
+      tmp->right = node;
+    }
+    node->parent = tmp;
+  }
+  if (!in)
+  {
+    clear(root);
+    throw std::invalid_argument("<INVALID INPUT>");
+  }
+  return root;
+}
+
+template< class T >
+BiTree< T >* rotate_right(BiTree< T >* root)
+{
+  if (!root)
+  {
+    throw std::invalid_argument("<INVALID ROTATE>");
+  }
+  if (!root->right)
+  {
+    throw std::invalid_argument("<INVALID ROTATE>");
+  }
+  BiTree< T >* new_root = root->left;
+  new_root->parent = root->parent;
+  root->parent = new_root;
+  root->left = new_root->right;
+  new_root->right = root;
+  return new_root;
+}
+
+template< class T >
+BiTree< T >* rotate_left(BiTree< T >* root)
+{
+  if (!root)
+  {
+    throw std::invalid_argument("<INVALID ROTATE>");
+  }
+  if (!root->left)
+  {
+    throw std::invalid_argument("<INVALID ROTATE>");
+  }
+  BiTree< T >* new_root = root->right;
+  new_root->parent = root->parent;
+  root->parent = new_root;
+  root->right = new_root->left;
+  new_root->left = root;
+  return new_root;
+}
+
+template< class T, class Comparator >
+BiTree< T >* find(BiTree< T >* root, const T& value, Comparator comp)
+{
+  if (!root)
+  {
+    return nullptr;
+  }
+  while (value != root->data)
+  {
+    if (comp(value, root->data))
+    {
+      if (!root->left)
+      {
+        return nullptr;
+      }
+      root = root->left;
+    }
+    else
+    {
+      if (!root->right)
+      {
+        return nullptr;
+      }
+      root = root->right;
+    }
+  }
+  return root;
+}
+
+template< class T >
+void clear(BiTree< T >* root)
+{
+  if (root)
+  {
+    clear(root->left);
+    clear(root->right);
+    delete root;
+    root = nullptr;
+  }
+}
+
+#endif
